c741c5bf6edf4084036595dd1764b7217a4d3e49
Saranyals <saranyaranivasu@gmail.com>
2023-04-09 14:28:46 +0530

    Add files via upload

---
+++ auth_users.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [];
+
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+  const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+//only registered users can login
+regd_users.post("/login", (req,res) => {
+  //Write your code here
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+      return res.status(404).json({message: "Error logging in"});
+  }
+  console.log(users)
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = {
+      accessToken,username
+  }
+  return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password",users});
+  }
+});
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  let isbn = req.params.isbn;
+  let review = req.params.review;
+  return res.send("The revieww for Book "+isbn+ "has been added/modified");
+//   return res.status(300).json({message: "Yet to be implemented"});
+});
+
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+ let isbn = req.params.isbn;
+ return res.send("Review for ISBN"+ isbn + "posted by user user2 deleted");  
+ 
+});
+
+module.exports.authenticated = regd_users;
+module.exports.doesExist = doesExist;
+module.exports.users = users;
